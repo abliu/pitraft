1c7a7f403e2485df172b27ffc201ac9213ab68f1
Jao-ke Chin-Lee <jchinlee94@gmail.com>
2014-04-29 14:34:15 -0400

    client, ish

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
   "fmt"
 //  "github.com/goraft/raft"
 //  "github.com/abliu/pitraft/pitServer"
+//  "github.com/abliu/pitraft/player"
 //  "github.com/abliu/pitraft/command"
 //  "github.com/abliu/pitraft/server"
   "os"
@@ -15,51 +16,103 @@ import (
 //  "encoding/gob"
   "strings"
   "encoding/json"
+  "math/rand"
 )
 
-/*var host string
-var port int*/
 var wrUrl string
+var rdUrl string
 
 func init() {
-  /*flag.StringVar(&host, "h", "localhost", "hostname")
-  flag.IntVar(&port, "p", 4002, "port")*/
-  flag.StringVar(&wrUrl, "u", "http://127.0.0.1:4002/", "leaderUrl")
+  flag.StringVar(&wrUrl, "u", "127.0.0.1:4002", "leaderUrl")
   flag.Usage = func() {
     fmt.Fprintf(os.Stderr, "Usage: %s [arguments] \n", os.Args[0])
     flag.PrintDefaults()
   }
 }
 
-
-
 func main() {
   flag.Parse()
 
-  /*wrConn, err := net.Dial("tcp", wrUrl)
-  if err != nil {
-    log.Fatal("Could not connect to leader", err)
-  }*/
+  uuid := rand.Int()
+  uuidStr := fmt.Sprintf("%d", uuid)
 
-  tmp := []string{wrUrl, "addPlayer", "/234"}
-  resp, err := http.Get(strings.Join(tmp, ""))
+  wrUrl = fmt.Sprintf("http://%s", wrUrl)
+  tmp := []string{wrUrl, "addPlayer", uuidStr}
+  resp, err := http.Get(strings.Join(tmp, "/"))
   fmt.Printf("%s::%v::%v\n\n", strings.Join(tmp, ""), *resp, err)
 
-  tmp = []string{wrUrl, "gameState"}
-  resp, err = http.Get(strings.Join(tmp, ""))
+  /*tmp = []string{wrUrl, "gameState", uuidStr}
+  resp, err = http.Get(strings.Join(tmp, "/"))
   fmt.Printf("%s::%#v::%#v\n\n", strings.Join(tmp, ""), *resp, err)
+fmt.Printf("3\n")*/
 
-  fmt.Printf("%v\n\n", (*resp).Body)
-  b := (*resp).Body
-  p := make([]byte, 1024, 1024)
-  n, err := b.Read(p)
-  fmt.Printf("%d::%s::%v\n\n", n, p, err)
+  var jsonResp map[string]int
+  fmt.Printf("%v::%v\n", json.NewDecoder((*resp).Body).Decode(&jsonResp), jsonResp)
+fmt.Printf("4\n")
 
-/*
-  encoder := gob.NewEncoder(wrConn)
-  p := command.NewWriteCommand(1, "barley", 2)
-  encoder.Encode(p)
-  wrConn.Close()
-*/
-  fmt.Println("done")
+  for {
+    fmt.Printf("> ")
+    var cmd string
+    fmt.Scanf("%s", &cmd)
+    //tokens := strings.Split(cmd, " ")
+    switch cmd {
+      case "propose":
+        // propose trade (resource, amt)
+        var resource string
+        var amount string
+        fmt.Printf("resource amt: ")
+        fmt.Scanf("%s %s", &resource, &amount)
+        fmt.Printf("\n")
+        tmp = []string{wrUrl, "propTrade", uuidStr, resource, amount}
+        resp, err = http.Get(strings.Join(tmp, "/"))
+        if err != nil {
+          fmt.Printf("Bad command %s %s %s::%s\n", cmd, resource, amount, err)
+          continue
+        }
+        //var tradeID int
+        b := (*resp).Body
+        p := make([]byte, 1024, 1024)
+        _, err := b.Read(p)
+        if err != nil {
+          fmt.Printf("Bad command %s %s %s::%s\n", cmd, resource, amount, err)
+          continue
+        }
+        //json.Unmarshal(p, &tradeID)
+        fmt.Printf("tradeID::%s::(%s, %s)\n", p, resource, amount)
+      case "cancel":
+        // cancel trade with given ID
+        var tradeID string
+        fmt.Printf("tradeID: ")
+        fmt.Scanf("%s", &tradeID)
+        fmt.Printf("\n")
+        tmp = []string{wrUrl, "cancelTrade", tradeID}
+        resp, err = http.Get(strings.Join(tmp, "/"))
+        if err != nil {
+          fmt.Printf("Bad command %s %s::%s\n", cmd, tradeID, err)
+          continue
+        }
+      case "getState":
+        // view my state
+        tmp = []string{wrUrl, "gameState", uuidStr}
+        resp, err = http.Get(strings.Join(tmp, "/"))
+        if err != nil {
+          fmt.Printf("Bad command::%s\n", err)
+          continue
+        } else {
+          json.NewDecoder((*resp).Body).Decode(&jsonResp)
+          fmt.Printf("%v\n", jsonResp)
+        }
+      case "view":
+        // view trades
+        tmp = []string{wrUrl, "allTrades"}
+        resp, err = http.Get(strings.Join(tmp, "/"))
+        if err != nil {
+          fmt.Printf("Bad command::%s\n", err)
+          continue
+        } else {
+          json.NewDecoder((*resp).Body).Decode(&jsonResp)
+          fmt.Printf("%v\n", jsonResp)
+        }
+    }
+  }
 }
